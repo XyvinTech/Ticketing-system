15593cd3630f260037844f9aa7dea08ce89fe51b
anjanavu <anjanavu2000@gmail.com>
2024-05-14 21:35:21 +0530

    updation

--- backend/utils/multerConfig.js
+++ backend/utils/multerConfig.js
@@ -1,5 +1,8 @@
 const multer = require("multer");
+const fileSizeLimit = 30 * 1024 * 1024; // 30 MB in bytes
 
+// Define a storage strategy for multer
+const storage = multer.memoryStorage();
 //~ Define a filter function for multer file uploads
 const filter = (req, file, cb) => {
   const allowedMimeTypes = [
@@ -8,17 +11,26 @@ const filter = (req, file, cb) => {
     "application/pdf",
     "video/mp4",
     "video/x-matroska", // For MKV files
-    "video/x-msvideo"   // For AVI files
+    "video/x-msvideo", // For AVI files
   ];
 
   if (allowedMimeTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Invalid file type, only JPEG, PNG, PDF, MP4, AVI, and MKV are allowed!"), false);
+    cb(
+      new Error(
+        "Invalid file type, only JPEG, PNG, PDF, MP4, AVI, and MKV are allowed!"
+      ),
+      false
+    );
   }
 };
 
 //~ Create a multer instance with the defined filter
-const upload = multer({ fileFilter: filter });
+const upload = multer({
+  storage: storage,
+  fileFilter: filter,
+  limits: { fileSize: fileSizeLimit },
+});
 
 module.exports = upload;
